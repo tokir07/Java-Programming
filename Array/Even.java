cc1037dcf06f7a3ef4ebd6c090a1bf1b55636feb
tokir07 <tokirkhan00291@gmail.com>
2026-04-11 22:09:21 +0530

    Added even.java file

--- Array/Even.java
+++ Array/Even.java
@@ -1,5 +1,29 @@
 package Array;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Even {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0; i < size; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println("Removing Even numbers from the array.........");
+        for (int i = 0; i < size; i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
+                i--;
+                size--;
+            }
+        }
+        System.out.println("Array after removing even numbers : " + list);
+        sc.close();
+    }
 
 }
